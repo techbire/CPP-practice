47f8e6eb547d9fe580d31a10fb3170ffc063c225
techbire <anshtechnical@gmail.com>
2024-06-06 22:52:51 +0530

    changes

--- function.cpp
+++ function.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// // sum of two number
+// sum of two number
 
 // int sum(int num1, int num2){
 //     int num3=num1+num2;
@@ -23,7 +23,7 @@ using namespace std;
 
 
 
-// // max finder
+// max finder
 
 // int maxx(int num1, int num2){
 //     if(num1>=num2){
@@ -46,19 +46,19 @@ using namespace std;
 
 // pass by reference
 
-void functionn(int arr[], int n){
-    arr[0]+=100;
-    cout<<"Value inside function: "<<arr[0]<<endl;
-}
+// void functionn(int arr[], int n){
+//     arr[0]+=100;
+//     cout<<"Value inside function: "<<arr[0]<<endl;
+// }
 
-int main(){
-    int n=5;
-    int arr[n];
-    for(int i=0;i<n;i=i+1){
-        cin>>arr[i];
-    }
-    functionn(arr,n);
+// int main(){
+//     int n=5;
+//     int arr[n];
+//     for(int i=0;i<n;i=i+1){
+//         cin>>arr[i];
+//     }
+//     functionn(arr,n);
 
-    cout<<"Value inside int main: "<<arr[0]<<endl;
-    return 0;
-}
+//     cout<<"Value inside int main: "<<arr[0]<<endl;
+//     return 0;
+// }
