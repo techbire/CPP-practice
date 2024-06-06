7b31ad99786cc925aab377401f49ec204fa57ebf
techbire <anshtechnical@gmail.com>
2024-06-06 16:59:17 +0530

    functions

---
+++ function.cpp
@@ -0,0 +1,50 @@
+// // sum of two number
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int sum(int num1, int num2){
+//     int num3=num1+num2;
+//     return num3;
+// }
+
+// int main(){
+//     int num1,num2;
+//     cin>>num1>>num2;
+//     int res=sum(num1,num2);
+//     cout<<res;
+//     return 0;
+
+// }
+
+
+
+
+
+
+
+// // max finder
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int maxx(int num1, int num2){
+//     if(num1>=num2){
+//         return num1;
+//     }
+//     return num2;
+// }
+
+// int main(){
+//     int num1, num2;
+//     cin>>num1>>num2;
+//     int maximum=maxx(num1, num2);
+//     cout<<maximum;
+// }
+
+
+
+
+
+
+// pass by refer
