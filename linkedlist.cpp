bbef7774ea14ae6d47ff27db22be9cf3753139c5
techbire <anshtechnical@gmail.com>
2024-09-04 22:10:07 +0530

    changes

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -61,31 +61,103 @@
 //why pointer -traversing easy hoti hai above wali se!
 
 
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// class Node{
+//     public:
+//     int val;
+//     Node* next;
+//     Node(int val){
+//         this->val=val;
+//         this->next=NULL;
+//     }
+// };
+
+// //method-3
+// void display(Node* head){
+//     Node* temp=head;
+//     while(temp!=NULL){
+//         cout<<temp->val<<" ";
+//         temp=temp->next;
+//     }
+// }
+
+
+// int main(){
+//     Node* a=new Node(10);
+//     Node* b=new Node(20);
+//     Node* c=new Node(30);
+//     Node* d=new Node(40);
+//     a->next=b;
+//     b->next=c;
+//     c->next=d;
+//     Node* temp=a;
+//     // while(temp!=NULL){
+//     //     cout<<temp->val<<" ";
+//     //     temp=temp->next;
+//     // }//method-1
+
+//     //method-2
+//     // cout<<a->next->next->next->val;
+
+// //method-3 ko recalling hoga
+// display(a);
+    
+// }
+
+
+
+//linkedlist class
+
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
+
+class Node {
+public:
     int val;
     Node* next;
-    Node(int val){
-        this->val=val;
-        this->next=NULL;
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
     }
 };
 
-int main(){
-    Node* a=new Node(10);
-    Node* b=new Node(10);
-    Node* c=new Node(10);
-    Node* d=new Node(10);
-    a->next=b;
-    b->next=c;
-    c->next=d;
-    Node* temp=a;
-    while(temp!=NULL){
-        cout<<temp->val<<" ";
-        temp=temp->next;
+class LinkedList {
+public: // Make the LinkedList constructor public
+    Node* head;
+    Node* tail;
+    int size;
+
+    LinkedList() { // Constructor is now public
+        head = tail = NULL;
+        size = 0;
     }
 
-    
+    void insertAtEnd(int val) {
+        Node* temp = new Node(val);
+        if (size == 0) {
+            head = tail = temp;
+        } else {
+            tail->next = temp;
+            tail = temp;
+        }
+        size++;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->val << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    LinkedList ll;
+    ll.insertAtEnd(10);
+    ll.insertAtEnd(20);
+    ll.display();
+    return 0;
 }
