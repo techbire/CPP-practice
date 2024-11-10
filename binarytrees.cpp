63b9f94a89ed48c4fdae8a25a7b03a1473316a6b
techbire <anshtechnical@gmail.com>
2024-11-10 16:44:42 +0530

    better version and inpostpre added

--- binarytrees.cpp
+++ binarytrees.cpp
@@ -1,24 +1,29 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
+
+class Node {
+public:
     int val;
     Node* left;
     Node* right;
-    Node(int val){
-        this->val=val;
-        this->left=nullptr;
-        this->right=nullptr;
-
+    Node(int val) {
+        this->val = val;
+        this->left = nullptr;
+        this->right = nullptr;
     }
 };
-void displayTree(Node* root){   
-    if(root==nullptr)return;
-    cout<<root->val<<" ";
+
+
+
+// Display the tree using Preorder traversal by default
+void displayTree(Node* root) {
+    if (root == nullptr) return;
+    cout << root->val << " ";
     displayTree(root->left);
     displayTree(root->right);
 }
 
+// Sum of all nodes in the tree
 // int sum(Node* root){
 //     if(root==nullptr)return 0;
 //     int leftSum=sum(root->left);
@@ -33,46 +38,96 @@ int sum(Node* root){
     return root->val+sum(root->left)+sum(root->right);
 }
 
-int size(Node* root){
-    if(root==nullptr)return 0;
-    return 1+size(root->left)+size(root->right);
+// Size of the tree (number of nodes)
+int size(Node* root) {
+    if (root == nullptr) return 0;
+    return 1 + size(root->left) + size(root->right);
 }
 
-int maxtree(Node* root){
-    if(root==nullptr)return INT_MIN;
-    int lmax=maxtree(root->left);
-    int rmax=maxtree(root->right);
-    return max(root->val,max(lmax,rmax));
-
+// Maximum value in the tree
+int maxtree(Node* root) {
+    if (root == nullptr) return INT_MIN;
+    int lmax = maxtree(root->left);
+    int rmax = maxtree(root->right);
+    return max(root->val, max(lmax, rmax));
 }
 
-int levels(Node* root){
-    if(root==nullptr)return 0;
-    return 1+max(levels(root->left),levels(root->right));    
+// Number of levels (height of the tree)
+int levels(Node* root) {
+    if (root == nullptr) return 0;
+    return 1 + max(levels(root->left), levels(root->right));
 }
 
+// Preorder Traversal (Root -> Left -> Right)
+void preorderTraversal(Node* root) {
+    if (root == nullptr) return;
+    cout << root->val << " ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
 
-//both sum function is ok
+// Inorder Traversal (Left -> Root -> Right)
+void inorderTraversal(Node* root) {
+    if (root == nullptr) return;
+    inorderTraversal(root->left);
+    cout << root->val << " ";
+    inorderTraversal(root->right);
+}
 
-int main(){
-    Node* a=new Node(1);  //root
-    Node* b=new Node(2);
-    Node* c=new Node(3);
-    Node* d=new Node(4);
-    Node* e=new Node(5);
-    Node* f=new Node(6);
-    Node* g=new Node(7);
+// Postorder Traversal (Left -> Right -> Root)
+void postorderTraversal(Node* root) {
+    if (root == nullptr) return;
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    cout << root->val << " ";
+}
 
-    a->left=b;
-    a->right=c;
-    b->left=d;
-    b->right=e;
-    c->left=f;
-    c->right=g;
-    displayTree(a);
-    cout<<endl;
-    cout<<sum(a)<<endl;
-    cout<<size(a)<<endl;
-    cout<<maxtree(a)<<endl;
-    cout<<levels(a)<<endl;
+int main() {
+    Node* a = new Node(1);  // root
+    Node* b = new Node(2);
+    Node* c = new Node(3);
+    Node* d = new Node(4);
+    Node* e = new Node(5);
+    Node* f = new Node(6);
+    Node* g = new Node(7);
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+
+//aisa hi dikhta hai 
+    //     1
+    //    / \
+    //   2   3
+    //  / \ / \
+    // 4  5 6  7
+
+
+
+    // Display additional tree properties
+    cout << "Sum of all nodes: " << sum(a) << endl;
+    cout << "Size of the tree (number of nodes): " << size(a) << endl;
+    cout << "Maximum value in the tree: " << maxtree(a) << endl;
+    cout << "Number of levels (height of the tree): " << levels(a) << endl;
+
+    // Display the tree using preorder traversal
+    cout << "Preorder Traversal: ";
+    preorderTraversal(a);
+    cout << endl;
+
+    // Display the tree using inorder traversal
+    cout << "Inorder Traversal: ";
+    inorderTraversal(a);
+    cout << endl;
+
+    // Display the tree using postorder traversal
+    cout << "Postorder Traversal: ";
+    postorderTraversal(a);
+    cout << endl;
+
+
+    return 0;
 }
