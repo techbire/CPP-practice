204cfe7bbfd068b6768b1fee8bd02b5834bbf492
techbire <anshtechnical@gmail.com>
2025-01-22 10:52:33 +0530

    added

--- recursion-practice.cpp
+++ recursion-practice.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 // Print an increasing-decreasing sequence using recursion
 // Example:
 // If n = 5, the output should be:
@@ -5,8 +8,7 @@
 
 
 // //normal approach
-// #include<bits/stdc++.h>
-// using namespace std;
+
 // int main(){
 //     int n;
 //     cin>>n;
@@ -18,23 +20,40 @@
 //     }
 // }
 
-//recursion approach
-#include <bits/stdc++.h>
-using namespace std;
+// //recursion approach
 
-void print(int x, int n){
-    if(x>=n){
-        cout<<n<<" ";
-        return;
-    }
-    cout<<x<<" ";
-    print(x+1,n);
-    cout<<x<<" ";
-}
 
-int main() {
-    int n;
-    cin >> n;
-    print(1,n);
-}
+// void print(int x, int n){
+//     if(x>=n){
+//         cout<<n<<" "; //4                                step2
+//         return;
+//     }
+//     cout<<x<<" ";  // eg.4 output from this line- 123    step1
+//     print(x+1,n);
+//     cout<<x<<" ";  //and from here 321                   step 3
+// }
 
+// int main() {
+//     int n;
+//     cin >> n;
+//     print(1,n);
+// }
+
+// //Write a program to print the sum of all odd numbers from a to b (inclusive) using recursion
+
+// void print(int x,int n){
+//     if(x>n)return;
+//     if(x%2!=0)cout<<x<<" ";
+//     print(x+1,n);
+// }
+
+
+// int main(){
+//     int a;
+//     cout<<"enter the first number: ";
+//     cin>>a;
+//     int b;
+//     cout<<"enter the second number: ";
+//     cin>>b;
+//     print(a,b);
+// }
